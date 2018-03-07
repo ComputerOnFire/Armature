9179234b0b56d6469015e9d68dfbf1a1d6d5653d
John Scully <scullyjw@gmail.com>
2018-03-07 02:40:49 -0600

    added buttons

--- app/src/main/java/com/computeronfire/armature/ui/CameraActivity.java
+++ app/src/main/java/com/computeronfire/armature/ui/CameraActivity.java
@@ -39,10 +39,10 @@ public class CameraActivity extends Activity implements PictureCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Integer contentArray[] = new Integer[4]; //TODO: Make sure you add to these when adding new armatures
-        contentArray[0] = R.layout.ct0;
-        contentArray[1] = R.layout.ct1;
-        contentArray[2] = R.layout.ct2;
-        contentArray[3] = R.layout.ct3;
+        contentArray[0] = R.layout.a0;
+        contentArray[1] = R.layout.a1;
+        contentArray[2] = R.layout.a2;
+        contentArray[3] = R.layout.a3;
 
         setContentView(contentArray[MainActivity.selector]);
         setResult(RESULT_CANCELED);
